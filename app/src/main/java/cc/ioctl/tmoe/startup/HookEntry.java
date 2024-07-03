40d0c680e656533a91edf0315d27ad0135647a2b
c1150945 <104805701+c1150945@users.noreply.github.com>
2024-07-04 04:34:00 +0800

    Update HookEntry.java

--- app/src/main/java/cc/ioctl/tmoe/startup/HookEntry.java
+++ app/src/main/java/cc/ioctl/tmoe/startup/HookEntry.java
@@ -15,6 +15,7 @@ public class HookEntry implements IXposedHookLoadPackage, IXposedHookZygoteInit 
     private static final List<String> TELEGRAM_CLIENT_PACKAGE_NAME_LIST = List.of(
             "org.telegram.messenger",
             "org.telegram.messenger.beta",
+            "tw.nekomimi.nekogram"
             "org.telegram.plus",
             "nekox.messenger",
             "com.jasonkhew96.pigeongram",
